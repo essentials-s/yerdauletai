40ff6fcf2021abfbc9f6c93205715c6af61a5253
essentials-s <esbergenerdaulet777@gmail.com>
2026-03-20 01:13:40 +0500

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,1 +1,18 @@
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
 
+    // заглушка (подключи БД позже)
+    if (email === "test@test.com" && password === "123456") {
+      return res.status(200).json({
+        user: {
+          email,
+          name: "Test User",
+          avatar: "https://i.pravatar.cc/150"
+        }
+      });
+    }
+
+    return res.status(401).json({ error: "Неверные данные" });
+  }
+}
